270bbb49c5c3c6d6e5def279438b3cbf1c97052a
jdejesu2 <jdejesu2@hawk.iit.edu>
2019-02-11 14:49:18 -0600

    entries of information

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.delete_all
+
+Product.create!(title: 'Lost Dog, literally',
+  description:
+    %{<p>
+      This is me always.
+      </p>},
+      image_url: 'dog1.jpg',
+      price: 100.00)
+
+Product.create!(title: 'Dog me up',
+  description:
+    %{<p>
+      Yes, this dog is busting your confindence,
+      but used the wrong your...
+      </p>},
+      image_url: 'dog2.jpg',
+      price: 1000.00)
+
+Product.create!(title: 'I am hungry as a dog',
+  description:
+    %{<p>
+      Fast food is not good for you.
+      </p>},
+      image_url: 'dog3.jpg',
+      price: 10000.00)
