bf2de75d62a06ca262ef3c1895db16dcb5e8b491
jdejesu2 <jdejesu2@hawk.iit.edu>
2019-02-23 21:43:38 -0600

    bin/rails db:migrate TO orders model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_11_201858) do
+ActiveRecord::Schema.define(version: 2019_02_24_033519) do
+
+  create_table "orders", force: :cascade do |t|
+    t.string "name"
+    t.text "address"
+    t.string "email"
+    t.integer "pay_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
