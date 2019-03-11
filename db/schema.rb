4e8af9e4a5e72d5bb1ba1a8827ecb67d169e98aa
jdejesu2 <jdejesu2@hawk.iit.edu>
2019-03-11 01:17:45 -0500

    rails db:migrate for new scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_11_201858) do
+ActiveRecord::Schema.define(version: 2019_03_11_061455) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
