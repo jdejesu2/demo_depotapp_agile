1e2e67e4fab9d98a60ba1de5a94c71163106f5b6
jdejesu2 <jdejesu2@hawk.iit.edu>
2019-03-15 09:45:26 -0500

    chapter 12.2 Atom feeds

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   #get 'store/index'
   root 'store#index', as: 'store_index'
 
-  resources :products
-  get 'say/hello'
-  get 'say/goodbye'
+  resources :products do
+    get :who_bought, on: :member
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
